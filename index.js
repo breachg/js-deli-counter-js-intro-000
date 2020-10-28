70feff153f8091870ba882a4860013a633c9734b
Garrett Breach <breachg1992@gmail.com>
2020-10-28 16:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(katzDeliLine,name) {
-  katzDeliLine.push(name)
+function takeANumber(katzDeliLine,newName) {
+  katzDeliLine.push(newName);
   return (`Welcome, $[name]. You are number $[KatzDeliLine] in line.`)
 }
