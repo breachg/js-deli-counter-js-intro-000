36e053754315c81e64dba556419452d634a73287
Garrett Breach <breachg1992@gmail.com>
2020-10-28 17:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine,newName) {
   katzDeliLine.push(newName);
-  return (`Welcome, ${katzDeliLine[0]}. You are number ${katzDeliLine[0]} in line.`)
+  return (`Welcome, ${katzDeliLine.[newName]}. You are number ${katzDeliLine.length} in line.`)
 }
