745df05b876dc38a21b02b57efceebe7c11b2acb
Garrett Breach <breachg1992@gmail.com>
2020-10-28 19:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var outputString;
   if (katzDeliLine == 0){
     outputString = "The line is currently empty."}
     else{
-      outputString = "The line is currently:"
+      outputString = "The line is currently: "
     for (var i=0; i < katzDeliLine.length;i++){
       if(i == 0){
         outputString=outputString +(i+1)+"."+katzDeliLine[i];
