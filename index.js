53bc4f9b03158eee847ba9efaf010afce6f12b84
Garrett Breach <breachg1992@gmail.com>
2020-10-28 18:01:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function takeANumber(katzDeliLine,newName) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length>0) {
-    return (`Currently serving ${katzDeliLine[0]}.`)
-    katzDeliLine.shift()
+  if (katzDeliLine.length == 0) {
+    return ("There is nobody waiting to be served!")
 }
-    else return ("There is nobody waiting to be served!")
+    else return (`Currently serving ${katzDeliLine[0]}.`)
+    katzDeliLine.shift()
 }
